fd22854dbc2ce70585371fa6c9d61678bdb76214
rebecca weeks <793716@SEQ.ORG>
2019-01-10 08:49:30 -0800

    sorting methods

--- lab 109/sort.js
+++ lab 109/sort.js
@@ -1,16 +1,47 @@
 function Sort (array){
-var minIndex;
+var maxIndex;
 var temp;
-for (var i < array.length; i++){
-  minIndex = i;
+for (var i < array.length; i--){
+  maxIndex = i;
   for (var j = i+1; j<array.length; j++){
-    if (array [j] < array [minIndex]){
-      minIndex = j;
+    if (array [j] > array [minIndex]){ //find largest value
+      maxIndex = j;
     }
   }
   temp = array [i];
-  array [i] = array [mindIndex];
-  array [minIndex] = temp; //swap
+  array [i] = array[maxIndex];
+  array [maxIndex] = temp; //swap
 }
 return array;
+}
+
+
+function BubbleSort (array)
+{
+    for (var i = array.length-1; i>0; i--){
+      for (var j = 0; j < i; j++){
+        if (array[j] >array[j+1]){
+          var temp = array[j];
+          array[j] =array[j+1];
+          array[j+1] = temp; //swap
+        }
+      }
+    }
+  }
+
+  function SelectionSort( array){
+    var minIndex;
+    var temp;
+    for (var i < array.length; i++){
+      minIndex = i;
+      for (var j = i+1; j<array.length; j++){
+        if (array [j] < array [minIndex]){
+          minIndex = j;
+        }
+      }
+      temp = array [i];
+      array [i] = array [mindIndex];
+      array [minIndex] = temp; //swap
+    }
+    return array;
     }
