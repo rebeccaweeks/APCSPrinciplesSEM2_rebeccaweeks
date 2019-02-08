3e498defd92f3d85ccb23e42283f4690f444d153
rebecca weeks <793716@SEQ.ORG>
2019-02-08 08:49:21 -0800

    graphs if hit play in debugger

--- lab 1101 json/bubblesort.js
+++ lab 1101 json/bubblesort.js
@@ -6,12 +6,14 @@ var millisecondEnd;
 var milliseondStart;
 var difference;
 var data;
+
 function setup(){
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 50);
   loadJSON("population2.json",getData);
-
     bubbleSort();
+console.log("Sorting hi");
+    barGraph();
   }
 
 function getData(d1){
@@ -21,6 +23,42 @@ data = d1;
 
 
 
+function barGraph(){
+
+console.log("Sorting bar graph");
+    //var data = [105, 212, 158, 31, 98, 54]; //change
+    var width = 200, // canvas width and height
+        height = 350,
+        margin = 20,
+        w = width - 2 * margin, // chart area width and height
+        h = height - 2 * margin;
+
+    var barWidth =  (h / data.countrydata.length) * 0.8; // width of bar
+    var barMargin = (h / data.countrydata.length) * 0.2; // margin between two bars
+
+    createCanvas(width, height);
+
+    textSize(14);
+
+    push();
+    translate(margin, margin); // ignore margin area
+
+    for(var i=0; i<data.countrydata.length; i++) {
+      push();
+      fill('steelblue');
+      noStroke();
+      translate(0, i* (barWidth + barMargin)); // jump to the top right corner of the bar
+      rect(0, 0, data.countrydata[i].females, barWidth); // draw rect
+
+      fill('#FFF');
+      text(data.countrydata[i].females + data.countrydata[i], 5, barWidth/2 + 5); // write data
+
+      pop();
+    }
+
+    pop();
+  }
+
 
 
 function draw(){
@@ -28,7 +66,7 @@ function draw(){
 
 //bubblesort function
 function bubbleSort() {
-
+  console.log("begin")
   millisecondStart = millis();
   var length = data.countrydata.length;
     for (var i = 0; i < length; i++) {
@@ -46,6 +84,7 @@ function bubbleSort() {
     }
     millisecondEnd = millis();
     difference = millisecondEnd - millisecondStart;
+    console.log("Sorting finished");
     console.log(data);
     console.log(millisecondStart + " start");
     console.log(millisecondEnd + " end");
