e4d0635fc74d3ba5fe93a2f6f2640c7180424f8b
rebecca weeks <793716@SEQ.ORG>
2019-02-14 08:47:40 -0800

    broke it

--- lab 1101 json/sketch.js
+++ lab 1101 json/sketch.js
@@ -1,6 +1,23 @@
-var data [];
-function preload(){
-  data = loadJSON ("population.json")
-
-}
+var data;
+var compares = 0;
 var swaps = 0;
+var millisecondEnd;
+var milliseondStart;
+var difference;
+var data;
+
+function setup(){
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 50);
+  loadJSON("population2.json",getData);
+    bubbleSort();
+    console.log("Sorting hi");
+    barGraph();
+  }
+
+function getData(d1){
+data = d1;
+}
+
+function draw(){
+}
